e179c315f984e5265da3b97935c739cc701bb277
Maxim Yakovlev <y.m.s.21@mail.ru>
2022-04-26 19:28:58 +0300

    init

---
+++ src/main/java/com/universal/UniversalApplication.java
@@ -0,0 +1,13 @@
+package com.universal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalApplication.class, args);
+	}
+
+}
